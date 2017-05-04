b28f51a2f2215cd9daef7c075730df80450e775e
orvice <orvice@gmail.com>
2017-05-04 09:31:00 +0800

    fix

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	log "github.com/Sirupsen/logrus"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func Init() {
@@ -12,8 +14,23 @@ func Init() {
 	InitCfg()
 }
 
+func waitSignal() {
+	var sigChan = make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGHUP)
+	for sig := range sigChan {
+		if sig == syscall.SIGHUP {
+		} else {
+			// is this going to happen?
+			log.Infof("caught signal %v, exit", sig)
+			os.Exit(0)
+		}
+	}
+}
+
 func main() {
 	Init()
 	go startTcpServer()
 	go startUdpServer()
+
+	waitSignal()
 }
