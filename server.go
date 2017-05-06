31d5c95d2ceeb99b3dc8e508d8ea0fbf40cb0c16
orvice <git@orx.me>
2017-05-06 21:43:59 +0800

    add update traffic

--- server.go
+++ server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	log "github.com/Sirupsen/logrus"
+	"github.com/catpie/musdk-go"
+	"strconv"
 	"strings"
 )
 
@@ -9,6 +11,7 @@ func handleLog(l string) error {
 	log.Info("get log: ", l)
 	arr := strings.Split(l, "\n")
 	log.Infof("len: %d ", len(arr))
+	var logs []musdk.UserTrafficLog
 	for _, v := range arr {
 		s := strings.Split(v, " ")
 		log.Debugf("processing  %s  len: %d", v, len(s))
@@ -23,7 +26,23 @@ func handleLog(l string) error {
 			}
 		}
 		log.Debugf("user %s  traffic %s", sArr[7], sArr[4])
-
+		u, err := strconv.Atoi(sArr[7])
+		if err != nil {
+			continue
+		}
+		d, err := strconv.Atoi(sArr[4])
+		if err != nil {
+			log.Error(err)
+		}
+		l := musdk.UserTrafficLog{
+			UserId: int64(u),
+			D:      int64(d),
+		}
+		logs = append(logs, l)
+	}
+	err := client.UpdateTraffic(logs)
+	if err != nil {
+		log.Errorf("error on update traffic  %s", err.Error())
 	}
 	return nil
 }
